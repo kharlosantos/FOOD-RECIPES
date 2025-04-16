4506731f4324c40a4ac9dfaec3b11e1d4f5206fd
Kharlo Jose V. Santos <khvi.santos.coc@phinmaed.com>
2025-04-16 10:14:14 +0800

    Create MyScript.js

---
+++ MyScript.js
@@ -0,0 +1,18 @@
+function toggleFood(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function homeFood(food) {
+    var content = document.getElementById(recipe);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
